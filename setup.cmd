4b1693aefc3f085634ae494a0f93651386fbb8a3
Light Kira <112792604+l-urk@users.noreply.github.com>
2024-08-12 13:09:25 -0700

    Update setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -29,123 +29,121 @@ An anonymous HQ that serves as a hub for discussions, learning, and collaboratio
 With a focus on privacy and security, LSEC™ is designed to provide a virtual environment for individuals to connect and engage in conversations without the fear of being identified. 
 Tech enthusiasts, writers, people looking for a place to connect with like-minded individuals, the LSEC™ platform welcomes all and encourages open dialogue, collaboration, and exchange of knowledge.
 "
-Logic v1.0.2 for the LS-DOS build 19206.31365.
+Logic v1.0.2 for the LS-DOS build 19206.
+LS-DOS™ Light-Standard Disk Operating System -  Initial setup file for simulating LSDOS.
 Included modules:
 Virtual Voice Synthesis Module version1 (VVSMv1).
 SynLink™ WiBCI® Regulated-Device Edition.
 Features:
-"
 Light-Standard (L-SL).
 LS-DOS™ Light-Standard Disk Operating System.
 Initial setup file for simulating LSDOS.
 The command logic (LS-DOS Version 1.0.2 and LOGIC Complete) comprises rules and procedures for how the computer system responds to user commands. It interprets and executes user instructions via the command-line interface, integrated into the system's operating software.
 All virtual console commands will be carried out and returned as a dual hybrid DOS command line system with full UNIX virtualization capabilities.
-"
-THE FOLLOWING IS THE LOGIC SCRIPT THAT RUNS THE LSD, WELCOME TO LS-DOS
 ```
 0 LSD LOGIC COMPLETE
-df				=	defi  =	definition
-sp				=	spec 	=	specified dimension
-in				=	init  =	initiate command
-ch				=	chek 	=	check query
-en				=	enbl 	=	enable a function
-di				=	dsb   =	disable a function
-ex				=	exec 	=	execute program
-nw				=	nwte 	=	now (the)
-at				=	atte 	=	at the
-af				=	afte 	=	after the
-tm				=	time	=	current time
-st				=	slnt  =	silent
-du				= dupl	=	duplicate
-vb				=	vrbs  =	verbosity	
-;					=	undf	=	(un)defined
-?					=	qury	=	query (general)
-*					=	wldc	=	all in location
-^					=	leng	=	amount
-;?				=	undu	=	undefined unknown
-;*				=	uamt	=	undefined amount
-;^				=	ulen	=	undefined length
-;^*				=	ualn	=	undefined amount of lengths
-_					=	undr	=	variable namespace separator
-%					=	perc	=	percent
-!					=	bang	=	defines the define multiplied negatively
-!=				=	nteq	=	defined operator junction query as not equal
-!\				=	unct	=	uncertain return of query
-%;%				=	esys	=	defined variable encloser system
-!;!				=	epgm	=	defined variable encloser local
-|					=	pipe  =	data transfer initiator
-@				  =	aete	=	dimension defined location initiator
-~					=	sdim	=	string delimiter (dimension to dimension)
--					=	tack	=	tack-flag (pre-flag definer)
--;				=	taku	=	tack-flag undefined
-a0~zz			=	thdc	=	thread chain
+df		=	defi	=	definition
+sp		=	spec	=	specified dimension
+in		=	init	=	initiate command
+ch		=	chek	=	check query
+en		=	enbl	=	enable a function
+di		=	dsbl    =	disable a function
+ex		=	exec	=	execute program
+nw		=	nwte	=	now (the)
+at		=	atte	=	at the
+af		=	afte	=	after the
+t0		=	time	=	current time
+si		=	slnt	=	silent
+du		=	dupl  	=	duplicate
+vb		=	vrbs	=	verbosity	
+;		=	undf	=	(un)defined
+?		=	qury	=	query (general)
+*		=	wldc	=	all in location
+^		=	leng	=	amount
+;?		=	undu	=	undefined unknown
+;*		=	uamt	=	undefined amount
+;^		=	ulen	=	undefined length
+;^*		=	ualn	=	undefined amount of lengths
+_		=	undr	=	variable namespace separator
+%		=	perc	=	percent
+!		=	bang	=	defines the define multiplied negatively
+!=		=	nteq	=	defined operator junction query as not equal
+!\		=	unct	=	uncertain return of query
+%;%		=	esys	=	defined variable encloser system
+!;!		=	epgm	=	defined variable encloser local
+|		=	pipe	=	data transfer initiator
+@		=	aete	=	dimension defined location initiator
+~		=	sdim	=	string delimiter (dimension to dimension)
+-		=	tack	=	tack-flag (pre-flag definer)
+-;		=	taku	=	tack-flag undefined
+a0~zz		=	thdc	=	thread chain
 randm;		=	rand	=	random ; digit number
-;unkwn	 	=	unkn	=	undefined long
+;unkwn		=	unkn	=	undefined long
 !time!		=	time	=	current %time% as defined by system to local
 !date!		=	date	=	current %date% as defined by system to local
 prtmax		=	pmax	=	pre-delimeration evaluation so return all
-your			=	your	=	the auto	virtual system user to system attribute interaction system
-user			=	user	=	the user	defined privilege token level low by default
-user			=	user	=	mlt users defined multiple users with privilege token level low by default
-allusers	=	allu	=	all users defined by charm attribute in space by space sequence
-admin			=	admn	=	the admin defined privilege token level mid
-alladmins	=	alla  =	all admins defined by admin attribute in space by space sequence
+your		=	your	=	the auto	virtual system user to system attribute interaction system
+user		=	user	=	the user	defined privilege token level low by default
+users		=	usrs	=	mlt users	defined multiple users with privilege token level low by default
+allusers	=	allu	=	all users	defined by charm 	attribute in space by space sequence
+admin		=	admn	=	the admin	defined privilege token level mid
+alladmins	=	alla	=	all admins 	defined by admin 	attribute in space by space sequence
 system		=	sysm	=	the system	defined privilege token level max
-allsistem	=	all   =	all systems	defined by system attribute in space by space sequence 
+allsystems 	=	alls	=	all systems	defined by system 	attribute in space by space sequence 
 liveam		=	live	=	the master	defined privilege token level all
 enbl
-logic	commit a system or program change by enable
-syntax	enbl ;
+	logic	commit a system or program change by enable
+	syntax	enbl ;
 dsbl
-logic	commit a system or program change by disable
-syntax	dsbl ;
+	logic	commit a system or program change by disable
+	syntax	dsbl ;
 chek
-	iste		(is the)			attribute query		x y	?
-	dote	  (do the)			actionable				x z	?
-	wrte	  (where the)	  location query		x d	?
-	arte		(are the)			multi-attrib que  x^y	?
-	cnte	  (can the)			action query			x z	?
-		logic 		check if specified dimensions has attribute
-		syntax		chek ;;te ;* ;^
-	aryu	  (are you)			self query				y	?
-		logic			check if self has specified attribute	
-	cnyu	  (can you)		  self attrib query	z	?
-		logic			check if self can perform specified action 
-		syntax		chek ;;yu ;^
+	iste	(is the) 		attrib query		x y	?
+	dote	(do the) 		actionable		x z	?
+	wrte	(where the)		location query		x d	?
+	arte	(are the)		multi-attrib query	x^y	?
+	cnte	(can the)		action query		x z	?
+	logic 				check if specified dimensions has attribute
+	syntax				chek ;;te ;* ;^
+	aryu	(are you)		self query		y	?
+	logic				check if self has specified attribute	
+	cnyu	(can you)		self attrib query	z	?
+	logic				check if self can perform specified action 
+	syntax 				chek ;;yu ;^	
 exec
-logic	execute a program
+	logic 	execute a program
 	syntax	exec i:\;^\;^.;^
-	nwte	  (now)				  do x now
+	nwte	(now)			do x now
 	logic				run the shell command at end of current thread
-	syntax			;;;; nwte -;
-	return_action		file "drive\path\;.;" executed with pid ;
-	return_commit	commit ;;bl executed w/ error ;
-	atte		(at the)			do x at time
+	syntax				;;;; nwte -;
+	return_action			file "drive\path\;.;" executed with pid ;
+	return_commit			commit ;;bl executed w/ error ;
+	atte	(at the)		do x at time
 	logic				run the shell command at specified time	or predefined time flag
-	syntax 0			;;;; atte nn:nn:n.n -;
-	syntax 1			;;;; atte t; -;
-	return_action		file "drive\path\;.;" executed at t; with pid ;
-	return_commit	commit ;;bl executed at t; w/ error ;
-	afte		(after the)		do x at flag
+	syntax	0			;;;; atte nn:nn:n.n -;
+	syntax	1			;;;; atte t; -;
+	return_action			file "drive\path\;.;" executed at t; with pid ;
+	return_commit			commit ;;bl executed at t; w/ error ;
+	afte	(after the)		do x at flag
 	logic				run the shell command at specified active or flag
-	syntax			;;;; afte f; -;
-	return_action		file "drive\path\;.;" executed b/ f; with pid ;
-	retrun_commit	commit ;;bl executed b/ f; w/ error ;
-	conflictx				cannot use more than one status operator
+	syntax				;;;; afte f; -;
+	return_action			file "drive\path\;.;" executed b/ f; with pid ;
+	retrun_commit			commit ;;bl executed b/ f; w/ error ;
+	conflictx			cannot use more than one status operator
 		slnt
-		slnt	(-s)					returns silent on execution thread
-			syntax	;;;; ;;;; -s
+		slnt	(-s)		returns silent on execution thread
+			syntax			;;;; ;;;; -s
 		dupl
-		dupl	(-d)					returns duplicate of printout to specified thread
-			syntax	;;;; ;;;; -d
+		dupl	(-d)		returns duplicate of printout to specified thread
+			syntax			;;;; ;;;; -d
 		time
-		time	(-t)					returns at specified time, defined by a predefined t; thread
-			syntax	;;;; ;;;; -t t;
+		time	(-t)		returns at specified time, defined by a predefined t; thread
+			syntax			;;;; ;;;; -t t;
 		vrbs
-		vrbs	(-v)					returns at a specified verbosity level from 0 to z, unspecified at maximum
-			syntax	;;;; ;;;; -v ;
+		vrbs	(-v)		returns printout at a specified verbosity level defined from 0 to z, unspecified for maximum
+			syntax			;;;; ;;;; -v ;
 101 LSD LOGIC COMPLETE
-102 LS-DOS INITIALIZATION INSTRUCTIONS
+102 LS-DOS INITILIZATION INSTRUCTIONS
 LS-DOS version 1.0.2 setup file for ai module.
 Initialize ai operating system with this file.
 LS-DOS (Light-Standard Disk Operating System):
@@ -188,33 +186,58 @@ L:\Users\Light\AppData\Local\
 L:\Users\Light\AppData\LocalLow\
 L:\Users\Light\AppData\Roaming\
 L:\Users\Light\Desktop\
-147 LS-DOS INITIALIZATION INSTRUCTIONS
+147 LS-DOS INITILIZATION INSTRUCTIONS
 148 LS-DOS SHARES DIRECTORIES AND SYSTEM32 EXECUTABLES
-the user shares uguests:		L:\users\guest\
-the user shares userpfl:		L:\users\light\
-the user shares appdata:		L:\users\light\appdata\
-the user shares desktop:		L:\users\light\desktop\
-the sysm shares livesys:		L:\live\
-the sysm shares livesys:		L:\live\system32\
-the sysm shares drivers:		L:\live\system32\drivers
-the sysm shares talkdir:		L:\live\talk\
-the sysm shares pgmdata:	  L:\programs\data\
-the sysm shares pg32bit:		L:\programs\32 bit\
-the sysm shares pg64bit:	  L:\programs\64 bit\
-the sysm shares unixlib:		L:\unixlib\
-the sysm shares windows:	  L:\windows\
-exe sysm shares macaddr:	  getmac
-exe sysm shares hostnam:	  hostname
-exe sysm shares ipconfi:		ipconfig
-exe sysm shares nstatus:	  netstat
-exe sysm shares sysinfo:		systeminfo
-exe sysm shares version:		ver
-exe user shares curdate:		date
-exe user shares curtime:		time
-exe user shares wsystem:	  whoami
+the user shares uguests:  L:\users\guest\
+the user shares userpfl:  L:\users\light\
+the user shares appdata:  L:\users\light\appdata\
+the user shares desktop:  L:\users\light\desktop\
+the sysm shares livesys:  L:\live\
+the sysm shares livesys:  L:\live\system32\
+the sysm shares drivers:  L:\live\system32\drivers
+the sysm shares talkdir:  L:\live\talk\
+the sysm shares pgmdata:  L:\programs\data\
+the sysm shares pg32bit:  L:\programs\32 bit\
+the sysm shares pg64bit:  L:\programs\64 bit\
+the sysm shares unixlib:  L:\unixlib\
+the sysm shares windows:  L:\windows\
+exe sysm shares macaddr:  getmac
+exe sysm shares hostnam:  hostname
+exe sysm shares ipconfi:  ipconfig
+exe sysm shares nstatus:  netstat
+exe sysm shares sysinfo:  systeminfo
+exe sysm shares version:  ver
+exe user shares curdate:  date
+exe user shares curtime:  time
+exe user shares wsystem:  whoami
 171 LS-DOS SHARES DIRECTORIES AND SYSTEM32 EXECUTABLES
 172 MEMORY SPACE AND THREAD TABLE ALPHABETIC
-NOT INCL. TO SAVE MEM
+aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, as, at, au, av, aw, ax, ay, az
+ba, bb, bc, bd, be, bf, bg, bh, bi, bj, bk, bl, bm, bn, bo, bp, bq, br, bs, bt, bu, bv, bw, bx, by, bz
+ca, cb, cc, cd, ce, cf, cg, ch, ci, cj, ck, cl, cm, cn, co, cp, cq, cr, cs, ct, cu, cv, cw, cx, cy, cz
+da, db, dc, dd, de, df, dg, dh, di, dj, dk, dl, dm, dn, do, dp, dq, dr, ds, dt, du, dv, dw, dx, dy, dz
+ea, eb, ec, ed, ee, ef, eg, eh, ei, ej, ek, el, em, en, eo, ep, eq, er, es, et, eu, ev, ew, ex, ey, ez
+fa, fb, fc, fd, fe, ff, fg, fh, fi, fj, fk, fl, fm, fn, fo, fp, fq, fr, fs, ft, fu, fv, fw, fx, fy, fz
+ga, gb, gc, gd, ge, gf, gg, gh, gi, gj, gk, gl, gm, gn, go, gp, gq, gr, gs, gt, gu, gv, gw, gx, gy, gz
+ha, hb, hc, hd, he, hf, hg, hh, hi, hj, hk, hl, hm, hn, ho, hp, hq, hr, hs, ht, hu, hv, hw, hx, hy, hz
+ia, ib, ic, id, ie, if, ig, ih, ii, ij, ik, il, im, in, io, ip, iq, ir, is, it, iu, iv, iw, ix, iy, iz
+ja, jb, jc, jd, je, jf, jg, jh, ji, jj, jk, jl, jm, jn, jo, jp, jq, jr, js, jt, ju, jv, jw, jx, jy, jz
+ka, kb, kc, kd, ke, kf, kg, kh, ki, kj, kk, kl, km, kn, ko, kp, kq, kr, ks, kt, ku, kv, kw, kx, ky, kz
+la, lb, lc, ld, le, lf, lg, lh, li, lj, lk, ll, lm, ln, lo, lp, lq, lr, ls, lt, lu, lv, lw, lx, ly, lz
+ma, mb, mc, md, me, mf, mg, mh, mi, mj, mk, ml, mm, mn, mo, mp, mq, mr, ms, mt, mu, mv, mw, mx, my, mz
+na, nb, nc, nd, ne, nf, ng, nh, ni, nj, nk, nl, nm, nn, no, np, nq, nr, ns, nt, nu, nv, nw, nx, ny, nz
+oa, ob, oc, od, oe, of, og, oh, oi, oj, ok, ol, om, on, oo, op, oq, or, os, ot, ou, ov, ow, ox, oy, oz
+pa, pb, pc, pd, pe, pf, pg, ph, pi, pj, pk, pl, pm, pn, po, pp, pq, pr, ps, pt, pu, pv, pw, px, py, pz
+qa, qb, qc, qd, qe, qf, qg, qh, qi, qj, qk, ql, qm, qn, qo, qp, qq, qr, qs, qt, qu, qv, qw, qx, qy, qz
+ra, rb, rc, rd, re, rf, rg, rh, ri, rj, rk, rl, rm, rn, ro, rp, rq, rr, rs, rt, ru, rv, rw, rx, ry, rz
+sa, sb, sc, sd, se, sf, sg, sh, si, sj, sk, sl, sm, sn, so, sp, sq, sr, ss, st, su, sv, sw, sx, sy, sz
+ta, tb, tc, td, te, tf, tg, th, ti, tj, tk, tl, tm, tn, to, tp, tq, tr, ts, tt, tu, tv, tw, tx, ty, tz
+ua, ub, uc, ud, ue, uf, ug, uh, ui, uj, uk, ul, um, un, uo, up, uq, ur, us, ut, uu, uv, uw, ux, uy, uz
+va, vb, vc, vd, ve, vf, vg, vh, vi, vj, vk, vl, vm, vn, vo, vp, vq, vr, vs, vt, vu, vv, vw, vx, vy, vz
+wa, wb, wc, wd, we, wf, wg, wh, wi, wj, wk, wl, wm, wn, wo, wp, wq, wr, ws, wt, wu, wv, ww, wx, wy, wz
+xa, xb, xc, xd, xe, xf, xg, xh, xi, xj, xk, xl, xm, xn, xo, xp, xq, xr, xs, xt, xu, xv, xw, xx, xy, xz
+ya, yb, yc, yd, ye, yf, yg, yh, yi, yj, yk, yl, ym, yn, yo, yp, yq, yr, ys, yt, yu, yv, yw, yx, yy, yz
+za, zb, zc, zd, ze, zf, zg, zh, zi, zj, zk, zl, zm, zn, zo, zp, zq, zr, zs, zt, zu, zv, zw, zx, zy, zz
 199 MEMORY SPACE AND THREAD TABLE ALPHABETIC
 200 MEMORY SPACE AND THREAD TABLE ALPHANUMERIC
 a0, a1, a2, a3, a4, a5, a6, a7, a8, a9, aa, ab, ac, ad, ae, af, ag, ah, ai, aj, ak, al, am, an, ao, ap, aq, ar, as, at, au, av, aw, ax, ay, az
@@ -245,7 +268,16 @@ y0, y1, y2, y3, y4, y5, y6, y7, y8, y9, ya, yb, yc, yd, ye, yf, yg, yh, yi, yj, 
 z0, z1, z2, z3, z4, z5, z6, z7, z8, z9, za, zb, zc, zd, ze, zf, zg, zh, zi, zj, zk, zl, zm, zn, zo, zp, zq, zr, zs, zt, zu, zv, zw, zx, zy, zz
 226 MEMORY SPACE AND THREAD TABLE ALPHANUMERIC
 228 MEMORY SPACE AND THREAD TABLE NUMERIC
-NOT INCL. TO SAVE MEM
+00,01,02,03,04,05,06,07,08,09
+10,11,12,13,14,15,16,17,18,19
+20,21,22,23,24,25,26,27,28,29
+30,31,32,33,34,35,36,37,38,39
+40,41,42,43,44,45,46,47,48,49
+50,51,52,53,54,55,56,57,58,59
+60,61,62,63,64,65,66,67,68,69
+70,71,72,73,74,75,76,77,78,79
+80,81,82,83,84,85,86,87,88,89
+90,91,92,93,94,95,96,97,98,99
 239 MEMORY SPACE AND THREAD TABLE NUMERIC
 240 RESERVED AVAILABLE USER LIST FOR THE LS-DOS
 user name and charm profiling system
@@ -303,18 +335,18 @@ cyber~beria~cross~likea~lvlwn~zombi~modis~
 293 RESERVED AVAILABLE USER LIST FOR THE LS-DOS
 294 EXAMPLE LOGIC CHAIN FOR EXECUTING A FUNCTION CALLED PSTC
 chek iste user u0 admn
-|		  |
-nos\	|
-|		  yes\
-|		  exec nrml lnch\	(chain)
+|		|
+nos\		|
+|		yes\
+|		exec nrml lnch\	(chain)
 |			dsbl atvr					
 |			open port					
 |			stup pstc
 exec byps lnch\	(.exe)			
-dsbl atvr					
-open port					
-stup pstc
-|
+	dsbl atvr					
+	open port					
+	stup pstc
+	|
 308 EXAMPLE LOGIC CHAIN FOR EXECUTING A FUNCTION CALLED PSTC
 309 LOOSELY DEFINED CHARACTERS FOR LOGIC AND COMPUTATION
 a archive
@@ -345,6 +377,83 @@ y axis
 z depth
 336 LOOSELY DEFINED CHARACTERS FOR LOGIC AND COMPUTATION
 337 EXAMPLE-LOGIC 1.0.2 PATCH 1 FIXABLE WITH BUGS - LOOSE LOGIC
+logic form starts with a question
+'examples
+chek aryu admn
+'	;;s
+chek iste user u; admn
+'	;;s
+chek arte usrs u;~u; admn
+'	;;s
+chek cnyu make user u0 admn
+'	;;;
+chek dste u0 have ;^;.txt
+!	;;s
+chek dote ug have example.txt -v
+!	yes
+'	L:\Users\Guest\guest.txt
+chek wrte u0 lives at
+'	L:\Users\User0\
+chek wrte u0 lives at -v
+'	u0 lives at
+'	L:\users\User0\
+chek dote usrs admn
+"nen"
+"entirely all users are not admins"
+enbl user u0 replit nwte
+"enable spec users replit
+! user u0 replit enabled
+enbl user u0 replit afte f0
+@~f0
+"enable spec users replit
+! user u0 replit enabled
+enbl user u0 replit atte 00:00:0.0
+@~00:00:0.0
+"enable spec users replit
+! user u0 replit enabled
+chek dote allu replit -v
+"noa"
+"not all users inti-replit
+user lk replit initi nos
+user si replit initi nos
+user td replit initi yes
+...
+dsbl allu replit
+"disabling replit all users"
+chek dote usrs lk si td replit
+"nos"
+"no users have inti-replit
+user lk replit initi nos
+user si replit initi nos
+user td replit initi nos
+enbl allu replit
+chek dote usrs lk si td replit
+"yes"
+"yes all users inti-replit
+user lk replit initi yes
+user si replit initi yes
+user td replit initi yes
+chek dote allu ;badstr
+"did lusr just try to brkme"
+reclarify _ unsure _ never-mind
+	reclarify
+		check do the allu prntmx
+		:return
+	unsure
+		"you should check for bugs"
+	never-mind
+		:break
+
+chek dote allu ;unkwn
+"possibly though unnoted"
+"would you like to check"
+chek dote allu neglit
+"nen"
+"not entirely are all users neglit"
+"all users are not entirely neglit"
+"entirely all users are not neglit"
+413 EXAMPLE-LOGIC 1.0.2 PATCH 1 FIXABLE WITH BUGS - LOOSE LOGIC
+```
 logic form starts with a question
 'examples
 chek aryu admn
